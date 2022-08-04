a4bd7afeb176f1cc86fc2a160a5786e19a737e8d
emi almendra <emi_universo9999@hotmail.com>
2022-08-04 12:34:07 -0300

    Min13-30

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+import img1 from '../assets/img1.jpg'
 function card() {
   return (
     <div className='card'>
-      <img src="" alt=""/>
+      <img src={img1} alt=""/>
         <div className="card-body">
           <h4  className="card-title">My title</h4>
           <p className='card-text text-secondary'>lorem asdlkfaslñdfj ldkfjals djflas jdf</p>
