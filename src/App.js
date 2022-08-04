2dade0608b36a50f1704b4c92b61e81dbe7b0f56
emi almendra <emi_universo9999@hotmail.com>
2022-08-04 09:20:51 -0300

    Gay

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/Card'
+function App() {
+  return (
+    <div>
+        <h1>
+            <Card/>
+        </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function card() {
+  return (
+    <div className='card'>
+      <img src="" alt=""/>
+        <div className="card-body">
+          <h4  className="card-title">My title</h4>
+          <p className='card-text text-secondary'>lorem asdlkfaslñdfj ldkfjals djflas jdf</p>
+        </div>
+    </div>
+  )
+}
+
+export default card
