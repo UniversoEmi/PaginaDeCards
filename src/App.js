9129395f0b78b3875522fd6c41788859ddcc3a18
emi almendra <emi_universo9999@hotmail.com>
2022-08-11 18:54:15 -0300

    UltimosPreparativos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Cards from './components/Cards'
-import './'
+import './App.css'
+
 function App() {
   
   return (
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ import Card from './Card'
 
 function Cards() {
   return (
-    <div className='container'>
+    <div className='container d-flex justify-content-center align-items-center h-100'>
         <div className="row">
             <div className="col-md-4">
               <Card/>
