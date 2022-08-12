2d3abdd3351db1697492e354784385736c419526
emi almendra <emi_universo9999@hotmail.com>
2022-08-12 14:43:54 -0300

    UltimosArreglos

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,12 @@ import React from 'react'
 import img1 from '../assets/img1.jpg'
 function card() {
   return (
-    <div className='card'>
+    <div className='card text-center bg-dark'>
       <img src={img1} alt=""/>
-        <div className="card-body">
+        <div className="card-body text-light">
           <h4  className="card-title">My title</h4>
           <p className='card-text text-secondary'>lorem asdlkfaslñdfj ldkfjals djflas jdf</p>
+          <a href='#!' className='btn btn-outline-secondary border-0'> Click Para Entrar</a>
         </div>
     </div>
   )
